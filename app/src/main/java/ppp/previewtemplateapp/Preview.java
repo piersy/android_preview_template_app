134f969c42051a19256204e3921964177174cf9a
piersy <pierspowlesland@gmail.com>
2017-02-04 22:18:46 +0000

    Fix preview aspect ratio and crashes
    The preview area is no longer skewed and the app seems to handle
    transitions without crashing or misbehaving.

--- app/src/main/java/ppp/previewtemplateapp/Preview.java
+++ app/src/main/java/ppp/previewtemplateapp/Preview.java
@@ -4,11 +4,13 @@ import android.content.Context;
 import android.graphics.ImageFormat;
 import android.hardware.Camera;
 import android.util.Log;
+import android.view.Display;
+import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
 import android.widget.Toast;
-
-import java.util.List;
 
 /**
  * Created by piers on 04/02/17.
@@ -17,11 +19,18 @@ import java.util.List;
 public class Preview implements SurfaceHolder.Callback {
 
     private final SurfaceHolder holder;
+    private SurfaceView previewSurface;
     private final Context context;
+    private WindowManager windowManager;
+    private LinearLayout layout;
+    private LinearLayout.LayoutParams lp;
 
-    public Preview(SurfaceView previewSurface, Context context) {
+    public Preview(SurfaceView previewSurface, Context context, WindowManager windowManager, LinearLayout layout) {
         holder = previewSurface.getHolder();
+        this.previewSurface = previewSurface;
         this.context = context;
+        this.windowManager = windowManager;
+        this.layout = layout;
         holder.addCallback(this);
     }
 
@@ -48,14 +57,14 @@ public class Preview implements SurfaceHolder.Callback {
     }
 
     public void surfaceCreated(SurfaceHolder holder) {
-        // no-op -- wait until surfaceChanged()
+        Log.d(TAG, "surfaceCreated: ");
+        camera = openFrontFacingCameraGingerbread();
     }
+
 
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
         Log.d(TAG, "surfaceChanged: ");
-        camera = openFrontFacingCameraGingerbread();
-        if (camera == null) return;
         Camera.Parameters params = camera.getParameters();
         if (params.getPreviewFormat() != ImageFormat.NV21) return;
         try {
@@ -65,35 +74,59 @@ public class Preview implements SurfaceHolder.Callback {
             Toast.makeText(context, t.getMessage(), Toast.LENGTH_LONG).show();
             return;
         }
-        Log.d(TAG, "Camera focus mode: " + params.getFocusMode());
-
-        Camera.Size size = getOptimalPreviewSize(params.getSupportedPreviewSizes(), width, height);
+        Camera.Size size = getOptimalPreviewSize2(params, width, height);
+        if (size == null) return;
         params.setPreviewSize(size.width, size.height);
         camera.setParameters(params);
 
         int bufferSize = ImageFormat.getBitsPerPixel(ImageFormat.NV21) * size.width * size.height / 8;
         camera.setPreviewCallbackWithBuffer(previewCallback);
         camera.addCallbackBuffer(new byte[bufferSize]);
-        camera.startPreview();
+        startPreview();
     }
 
+    // same for stopping the preview
+    public void stopPreview() {
+        if (camera != null) {
+            camera.stopPreview();
+        }
+    }
+
+    public void startPreview() {
+        if (camera != null) {
+           // previewSurface.setLayoutParams(lp);
+            camera.startPreview();
+        }
+    }
     public void surfaceDestroyed(SurfaceHolder holder) {
-        // no-op
+        if (camera != null) {
+            stopPreview();
+            camera.release();
+            camera = null;
+        }
     }
 
     /**
      * Should be called when app is paused, allows the Preview to stop and release the camera.
      */
-    public void onPause(){
-        camera.stopPreview();
-        camera.release();
+    public void onPause() {
+        Log.d(TAG, "onPause: ");
+        stopPreview();
+    }
+
+    /**
+     * Should be called when app is paused, allows the Preview to stop and release the camera.
+     */
+    public void onResume() {
+        Log.d(TAG, "onResume: ");
+        startPreview();
     }
 
     Camera.PreviewCallback previewCallback = new Camera.PreviewCallback() {
 
         public void onPreviewFrame(byte[] data, Camera camera) {
-            Log.i(TAG, "onPreviewFrame: ");
-            camera.addCallbackBuffer(data);
+            //Log.i(TAG, "onPreviewFrame: ");
+            //camera.addCallbackBuffer(data);
 //                Rect rect = new Rect(0, 0, camera.getParameters().getPreviewSize().width, camera.getParameters().getPreviewSize().height);
 //                YuvImage img = new YuvImage(data, ImageFormat.NV21, rect.width(), rect.height(), null);
 //                OutputStream outStream = null;
@@ -116,46 +149,73 @@ public class Preview implements SurfaceHolder.Callback {
     };
 
 
-    private Camera.Size getOptimalPreviewSize(List<Camera.Size> sizes, int w, int h) {
-   Log.e(TAG,"SurfaceWidth:"+w);
-    Log.e(TAG,"SurfaceHeight:"+h);
-    final double ASPECT_TOLERANCE = 0.05;
-    double targetRatio = (double) w/h;
+    //This optimal size makes sure we can display a 1:1 ratio of preview
+    //pixels to view pixels. It does this by finding a preview with a small
+    //dimension equal to the view's small dimension with the preview large dimension
+    //smaller than that of the view.
+    private Camera.Size getOptimalPreviewSize2(Camera.Parameters params, int w, int h) {
+        Log.e(TAG, "SurfaceWidth:" + w);
+        Log.e(TAG, "SurfaceHeight:" + h);
 
-    if (sizes==null) return null;
+        Camera.Size optimalSize = null;
 
-    Camera.Size optimalSize = null;
+        int vs = Math.min(w, h);
+        int vl = Math.max(w, h);
+        int cs=0, cl= 0;
 
-    double minDiff = Double.MAX_VALUE;
+        int targetHeight = h;
 
-    int targetHeight = h;
+        // Find size
+        for (Camera.Size size : params.getSupportedPreviewSizes()) {
+                Log.e(TAG, "Width:" + size.width);
+                Log.e(TAG, "Height:" + size.height);
+            cs = Math.min(size.width, size.height);
+            cl = Math.max(size.width, size.height);
+            if (cs == vs && cl <= vl) {
 
-    // Find size
-    for (Camera.Size size : sizes) {
-      Log.e(TAG,"--------------------");
-      Log.e(TAG,"Width:"+size.width);
-      Log.e(TAG,"Height:"+size.height);
-      double ratio = (double) size.width / size.height;
-      if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE) continue;
-      if (Math.abs(size.height - targetHeight) < minDiff) {
-        optimalSize = size;
-        minDiff = Math.abs(size.height - targetHeight);
-      }
-    }
+                optimalSize = size;
+                break;
+            }
 
-    if (optimalSize == null) {
-      minDiff = Double.MAX_VALUE;
-      for (Camera.Size size : sizes) {
-        if (Math.abs(size.height - targetHeight) < minDiff) {
-          optimalSize = size;
-          minDiff = Math.abs(size.height - targetHeight);
         }
-      }
-    }
-    Log.e(TAG,"--------------------");
-    Log.e(TAG,"OptimalWidth:"+optimalSize.width);
-    Log.e(TAG,"OptimalHeight:"+optimalSize.height);
-    return optimalSize;
+        if (optimalSize == null) return null;
+        // If the view is already the right size then don't set the layout params
+        // otherwise we get stuck in a loop since setting the layout params causes the
+        // surface to be changed.
+        if(vs != cs || vl !=cl) {
+            //Now padd the surface view so that the preview is in 1:1 ratio
+            lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            lp.setMargins(vl - cl, 0, 0, 0);
+            previewSurface.setLayoutParams(lp);
+        }
+        return optimalSize;
     }
 
+
+    public void orientCamera(Camera camera, int w, int h) {
+
+        Camera.Parameters parameters = camera.getParameters();
+
+        Display display = windowManager.getDefaultDisplay();
+
+        if (display.getRotation() == Surface.ROTATION_0) {
+            parameters.setPreviewSize(h, w);
+            camera.setDisplayOrientation(90);
+        }
+
+        if (display.getRotation() == Surface.ROTATION_90) {
+            parameters.setPreviewSize(w, h);
+        }
+
+        if (display.getRotation() == Surface.ROTATION_180) {
+            parameters.setPreviewSize(h, w);
+        }
+
+        if (display.getRotation() == Surface.ROTATION_270) {
+            parameters.setPreviewSize(w, h);
+            camera.setDisplayOrientation(180);
+        }
+
+        camera.setParameters(parameters);
+    }
 }
--- app/src/main/java/ppp/previewtemplateapp/PreviewActivity.java
+++ app/src/main/java/ppp/previewtemplateapp/PreviewActivity.java
@@ -3,11 +3,13 @@ package ppp.previewtemplateapp;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
 
 public class PreviewActivity extends AppCompatActivity {
     private final String TAG = "PreviewActivity";
@@ -27,15 +29,20 @@ public class PreviewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_preview);
 
         SurfaceView view = (SurfaceView) findViewById(R.id.activity_preview);
+        LinearLayout layout = (LinearLayout) findViewById(R.id.activity_layout);
         // Create the preview, it will take care of managing the preview display
-        preview = new Preview(view, this);
+        preview = new Preview(view, this, getWindowManager(), layout);
     }
 
     @Override
     protected void onPause() {
         preview.onPause();
         super.onPause();
-
     }
 
+    @Override
+    protected void onResume() {
+        preview.onResume();
+        super.onPause();
+    }
 }
